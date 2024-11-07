bf5de6211c1db7921ce8cdb93e3e63f598fa41c3
Muhammad Moneib <mmoneib@gmx.com>
2024-11-08 00:11:09 +0100

    Added documentation to random_actions.

--- random_actions.py
+++ random_actions.py
@@ -1,24 +1,86 @@
-#!/usr/bin/emv python3
+#!/usr/bin/env python3
+"""
+Random Actions
+
+A collection of functions used to provide convenicence and consolidation of random decisions.
+"""
 import argparse
 import random
 import math
 
+#TODO Add possibility to use parameters for triggering a sequence of actions.
+#TODO Add an action to generate truth tables.
+
 def toss_coin():
+  """
+  Simulates a fair coin toss.
+   
+  Returns:
+    A string in the form of "H" for head or "T" for  tail.
+  """
   return randomly_choose(options=["H","T"])
 
+def left_or_right:
+  """
+  Picks left or right direction randomly.
+
+  Returns:
+    A string in the form of "L" for left or "R" for right.
+  """
+  return randomly_choose(options=["L","R"])
+
+def up_or_down:
+  """
+  Picks up or down movement randomly.
+
+  Returns:
+    A string in the form of "U" for up and "D" for down.
+  """
+  return randomly_choose(options=["U","D"])
+
 def roll_dice():
+  """
+  Simulates a roll of a dice.
+
+  Returns:
+    An integer from 1 to 6.
+  """
   return randomly_choose(range(1,7))
 
 def play_russian_roullette():
+  """
+  Simulates a Russian Roullete game using a pistol with a capacity of 6 bullets and is only loaded with 1.
+
+  Returns:
+    The string "CLICK" 1/6 of the times signifying a miss, or the string "BANG" 5/6 of the tumes signifying a hit. 
+  """
   return randomly_choose_with_bias(options=["CLICK", "BANG"], biases=[5, 1])
 
 def navigate_randomly():
+  """
+  Simulates a random navigator which gives back one of the eight main directions from a compass.
+
+  Returns:
+    The string "N" for north, "NE" for north ease, "E" for east, "SE" for south east, "S" for south, "SW" for south west, "W" for west, or "NW" for morth west. 
+  """
   return randomly_choose(options=["N", "NE", "E", "SE", "S", "SW", "W", "NW"])
   
 def move_randomly():
+  """
+  Simulates a random navigator which gives back one of the four main directions from a compass.
+
+  Returns:
+    The string "N" for north, "E" for east, "S" for south, or "W" for west. 
+  """
   return randomly_choose(options=["N", "S", "E", "W"])
 
 def turn_randomly():
+  """
+  Simulates a random planar movement or rotation with respect to the object itself rather than the main directions of a compass.
+
+  Returns:
+    The string "F" for forward, "B" for backward, "L" for leftward, or "R" for rightward. 
+  """
   return randomly_choose(options=["F", "B", "L", "R"])
 
 def randomly_choose_with_bias(options=[], biases=[]):
@@ -38,9 +100,30 @@ def randomly_choose_with_bias(options=[], biases=[]):
   return random.choice(biasedOptions)
 
 def randomly_choose(options=[]):
+  """
+  A simple random choice out of the members of the supplied list.
+
+  Args:
+    options (dictionary): The list of options from which a random choice will be done.
+
+  Returns:
+    An object as chosen from the list. 
+
+  """
   return random.choice(options)
 
-def random_sequence(targetAction="", delimiter="\n", sequenceSize=1):
+def random_sequence_of_actions(targetAction="", delimiter="\n", sequenceSize=1):
+  """
+  Calls another action a specified number of times to get a random sequence of outputs seperated by the specified delimiter.
+
+  Args:
+    targetAction (str): The name of the action (function) within this script to be called and for which the sequence of output is expected.
+    delimiter (str): The delimiter to be used to separate betwee the output of each iteration. Defaults to a new line.
+    sequenceSize (int): The number of iterations through which the action will be called.
+
+  Returns:
+    A string containin the sequence of outputs from the action separated by the delimiter.
+  """
   result=""
   for i in range(0, sequenceSize):
     result += eval(targetAction+"()")
@@ -49,7 +132,8 @@ def random_sequence(targetAction="", delimiter="\n", sequenceSize=1):
   return result
 
 def histogram(targetAction="", sequenceSize=1):
-  print
+  print("Not implemented.")
+
 
 if __name__ == "__main__":
   functionsList = [func for func in dir() if callable(eval(func)) and not func.startswith("__")]
